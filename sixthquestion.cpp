c0005d2eb5f572c496e2b6b5944ee94aadbc01bd
Kowshiq Kattamuri <kattamuri_b201298bt@nitc.ac.in>
2023-06-26 20:38:09 +0530

    Create sixthquestion.cpp

---
+++ sixthquestion.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int start = 0, end = nums.size() - 1;
+    while(start <= end){
+       int mid = start + (end - start)/2;
+       if(nums[mid] == target){
+           return mid;
+       }else if(nums[mid] > target){
+           end = mid - 1;
+       }else{
+           start = mid + 1;
+       }
+    }
+  return -1;
+}
+
+int main(){
+  int n, m;
+  cin >> n >> m;
+  vector<int> nums(n);
+  for(int i = 0; i < n; i++){
+      cin >> nums[i];
+  }
+  cout << search(nums, m) << endl;
+}
